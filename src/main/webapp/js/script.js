2aab04726bf164513b13ce15ce4608ffee6bca7f
dudu18xb <eduardorocha460@gmail.com>
2017-06-11 01:26:35 -0300

    adicionando o boostrap

---
+++ src/main/webapp/js/script.js
@@ -0,0 +1,11 @@
+$(function() {
+	
+	// Call the plugin
+	
+	$('#main').tumblrNewsTicker({
+		time: 5000,
+		title:  'Agenda Semanal',
+		blog: 'http://amazing-jquery-plugins.tumblr.com/'
+	});	
+				
+});
